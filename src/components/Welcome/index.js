c40fa4ca6c5911e39d826bd9f4f797dd5eda0120
VIJAYKUMAR <115571763+vijaykumardumpeti@users.noreply.github.com>
2022-11-11 21:29:53 +0530

    Update index.js

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,20 +1,38 @@
 // Write your code here
-import Component from 'react'
+
+import {Component} from 'react'
 import './index.css'
 
 class Welcome extends Component {
+  state = {
+    isSubscribed: false,
+  }
+
+  btnFunction = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
+
   render() {
+    const {isSubscribed} = this.state
+
+    const btnText = isSubscribed ? 'Subscribed' : 'Subscribe'
+
     return (
       <div className="container">
         <h1 className="heading">Welcome</h1>
         <p className="paragraph">Thank you!Happy Learning</p>
         <div>
-          <button className="btn-style" type="button">
-            Subscribe
+          <button
+            onClick={this.btnFunction}
+            className="btn-style"
+            type="button"
+          >
+            {btnText}
           </button>
         </div>
       </div>
     )
   }
 }
+
 export default Welcome
